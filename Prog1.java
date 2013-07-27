57da299da9c8000734e1831668a998643aa49710
Josh L <josh.lindoo@eprize.com>
2013-07-27 14:43:44 -0400

    Initial Commit with some basic program structure

---
+++ Prog1.java
@@ -0,0 +1,27 @@
+
+public class Prog1 {
+	//global vars
+	public boolean debug = false; //toggles debug mode
+	
+	public static void main(String[] args) {
+		//local vars
+		int numArgs = args.length; //number of args
+		
+		//check arguments
+		if(numArgs == 1) {
+			//args as filename
+			
+		} else if(numArgs > 2) {
+			//invalid number of args
+			
+		} else {
+			//args as -dubug flag and filename
+			
+		}
+		
+		
+	}
+	public static void blah() {
+		
+	}
+}
